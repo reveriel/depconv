592c267f228553f20a27d998d4bd56344bb1c2c6
Guo.Xing <reveriel@hotmail.com>
2020-08-22 22:43:40 +0800

    depconv84, 64x512x512, smaller delta theta

--- second/run.sh
+++ second/run.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=$(pwd)/../:$PYTHONPATH
 
 # python3 ./pytorch/train.py train --config_path=./configs/car.fhd.config --model_dir=second1 --resume=True
 
-CUDA_VISIBLE_DEVICES=0,1 \
+CUDA_VISIBLE_DEVICES=0 \
 python3 ./pytorch/train.py train  \
- --config_path=configs/car.fhd.config --model_dir=depconv79 --resume=True\
- --multi_gpu=True
+ --config_path=configs/car.fhd.config --model_dir=depconv84 --resume=False\
+ --multi_gpu=False
 
 #CUDA_VISIBLE_DEVICES=2 python3 ./pytorch/train.py evaluate  \
  #--config_path=configs/car.fhd.config --model_dir=depconv26
