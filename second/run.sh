fcd22cd351386371000ba6e829f1add5a3d73a1f
Guo.Xing <reveriel@hotmail.com>
2020-08-24 17:40:32 +0800

    depconv92, shuffle points, based 91

--- second/run.sh
+++ second/run.sh
@@ -7,7 +7,7 @@ export PYTHONPATH=$(pwd)/../:$PYTHONPATH
 
 CUDA_VISIBLE_DEVICES=0 \
 python3 ./pytorch/train.py train  \
- --config_path=configs/car.fhd.config --model_dir=depconv91 --resume=True\
+ --config_path=configs/car.fhd.config --model_dir=depconv92 --resume=False\
  --multi_gpu=False
 
 #CUDA_VISIBLE_DEVICES=2 python3 ./pytorch/train.py evaluate  \
